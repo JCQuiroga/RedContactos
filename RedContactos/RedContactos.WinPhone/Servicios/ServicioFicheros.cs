d84c59b863479827cdb0179485e534cbab25c025
JCQuiroga <josecquiroga@gmail.com>
2016-01-29 14:27:58 +0100

    -<|>-

--- RedContactos/RedContactos.WinPhone/Servicios/ServicioFicheros.cs
+++ RedContactos/RedContactos.WinPhone/Servicios/ServicioFicheros.cs
@@ -1,33 +1,50 @@
 ﻿using System;
 using System.IO;
-using RedContactos.Droid.Servicios;
+using System.Threading.Tasks;
+using Windows.Storage;
 using RedContactos.Servicios;
+using RedContactos.WinPhone.Servicios;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(ServicioFicheros))]
-namespace RedContactos.Droid.Servicios
+namespace RedContactos.WinPhone.Servicios
 {
     public class ServicioFicheros : IServicioFicheros
     {
-        public void GuardarTexto(string texto, string fichero)
+        public async void GuardarTexto(string texto, string fichero)
         {
-            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            var rutafinal = Path.Combine(path, fichero);
-            File.WriteAllText(rutafinal, texto);
+            var carpeta = ApplicationData.Current.LocalFolder;
+            var f = await carpeta.CreateFileAsync(fichero, CreationCollisionOption.ReplaceExisting);
+            using (var stream = new StreamWriter(await f.OpenStreamForWriteAsync()))
+            {
+                stream.Write(texto);
+            }
+        }
+
+        public async Task<string> CargarTexto(string fichero)
+        {
+            try
+            {
+                var carpeta = ApplicationData.Current.LocalFolder;
+                var f = await carpeta.GetItemAsync(fichero);
+                using (var stream = new StreamReader(f.Path))
+                {
+                    var txt = stream.ReadToEnd();
+                    return txt;
+                }
+            }
+            catch (Exception)
+            {
+                return "";
+                ;
+            }
         }
 
         public string RecuperarTexto(string fichero)
         {
-            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            var rutafinal = Path.Combine(path, fichero);
-            try
-            {
-                return File.ReadAllText(rutafinal);
-            }
-            catch (Exception)
-            {
-                return null;
-            }
+            var data = CargarTexto(fichero);
+            data.Wait();
+            return data.Result;
         }
     }
 }
